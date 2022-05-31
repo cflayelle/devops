9b08b32ebd433acf065f641c35dc68fb10d21cd6
Abdelilah LEZAR <abdelilahlezar@gmail.com>
2022-05-31 15:19:52 +0200

    Cards responsive

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,12 @@ import CardComponent from "../components/CardComponent";
 
 export default function Home() {
     return (
-        <div className="container cards d-flex gap-3">
-            <div className="cards d-flex gap-3 mt-5">
-                <CardComponent bgColor='bg-primary' text='Primary Card'></CardComponent>
-                <CardComponent bgColor='bg-warning' text='Warning Card'></CardComponent>
-                <CardComponent bgColor='bg-success' text='Success Card'></CardComponent>
-                <CardComponent bgColor='bg-danger' text='Danger Card'></CardComponent>
+        <div className="container">
+            <div className="cards d-flex gap-3 mt-5 flex-column flex-lg-row">
+                <CardComponent bgColor='bg-primary' text='Primary Card'/>
+                <CardComponent bgColor='bg-warning' text='Warning Card'/>
+                <CardComponent bgColor='bg-success' text='Success Card'/>
+                <CardComponent bgColor='bg-danger' text='Danger Card'/>
             </div>
         </div>
     )
