ad351ddb945d89c07ade745b7768ee11b04b51b4
Flayelle Camille <camille@deCamille.local>
2022-06-01 14:00:39 +0200

    graph

--- components/AreaChart.js
+++ components/AreaChart.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import {Bar} from 'react-chartjs-2';
-import { Chart as ChartJS } from 'chart.js/auto'
+import {Line} from 'react-chartjs-2';
 
 
 const state = {
@@ -9,6 +8,8 @@ const state = {
   datasets: [
     {
       label: 'Rainfall',
+      fill: false,
+      lineTension: 0.5,
       backgroundColor: 'rgba(75,192,192,1)',
       borderColor: 'rgba(0,0,0,1)',
       borderWidth: 2,
@@ -21,20 +22,20 @@ const state = {
 const AreaChart = (props) => {
   return (
     <div>
-      <Bar
-        data={state}
-        options={{
-          title:{
-            display:true,
-            text:'Average Rainfall per month',
-            fontSize:20
-          },
-          legend:{
-            display:true,
-            position:'right'
-          }
-        }}
-      />
+      <Line
+          data={state}
+          options={{
+            title:{
+              display:true,
+              text:'Average Rainfall per month',
+              fontSize:20
+            },
+            legend:{
+              display:true,
+              position:'right'
+            }
+          }}
+        />
     </div>
   );
 }
